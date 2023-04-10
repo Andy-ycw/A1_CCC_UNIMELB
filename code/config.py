5fda5cbcee0b8094a47641380cf454026455fa74
Yu Chiawei <andyiao19960607@gmail.com>
2023-04-11 00:52:55 +1000

    Content of submission

---
+++ code/config.py
@@ -0,0 +1,9 @@
+CONFIG = (
+    # Files for testing purpose
+    # "./data/smallTwitter.json",
+    # "./data/twitter-data-small.json",
+
+    "./data/bigTwitter.json", # file_path
+    "./data/sal.json", # sal.json
+    9, # counts_array_size - according to the format in notebook, it's the number of fields necessary for recording total counts and capital counts.
+)
